2fd4eea6497247331fab3d322197a17422b0a5f4
yosukemaeda <yoke1390@outlook.jp>
2024-03-16 19:51:12 +0900

    Ver2.3完成！軽微な修正、コメントの追加

--- ver2.3.js
+++ ver2.3.js
@@ -1,14 +1,13 @@
 class Member {
     constructor(name) {
         this.name = name;
+        this.nickname = name; // ニックネームの初期値を本名にする
+        this.messages = [];
+        this.slides = [null, null];
 
         const note1 = name + "1枚目。このテキストは変更しないでください。"; // 1枚目のスライドの識別子（スピーカーノート）
         const note2 = name + "2枚目。このテキストは変更しないでください。"; // 2枚目のスライドの識別子（スピーカーノート）
         this.notes = [note1, note2];
-
-        this.nickname = name; // ニックネームの初期値を本名にする
-        this.messages = [];
-        this.slides = [null, null];
     }
 }
 
@@ -22,10 +21,11 @@ function main() {
     // データの処理 ///////////////////////////////////////////////////
     log("アンケート結果の取得中...");
     const { member_name_source, nickname_source, message_source } = fetchKanfiData(main_sheet);
+
     log("メンバーの一覧を作成中...");
     const members = makeMembersMap(member_name_source);
-    log("読んでほしい名前を読み込み中...");
-    setNickNames(nickname_source, members);
+    log("呼んでほしい名前を割り当て中...");
+    setNicknames(nickname_source, members);
     log("メッセージを割り当て中...");
     setMessages(message_source, members);
 
@@ -33,46 +33,50 @@ function main() {
     log("感フィを作成するスライドと接続中...");
     const kanfi_slide = connectKanfiSlide(main_sheet);
     log("既存のスライドを割り当て中...");
-    assignExsistingSlidestoMembers(members, kanfi_slide);
+    assignExsistingSlidesToMembers(members, kanfi_slide);
+
     for (const member of members.values()) {
         log(member.name + "の1枚目のスライドを処理中...");
         firstSlide(member, kanfi_slide);
         log(member.name + "の2枚目のスライドを処理中...");
         secondSlide(member, kanfi_slide);
     };
-    //  TODO: スライドの順番を変更
-    // sortSlides(kanfi_slide);
+
+    log("処理完了");
 }
 
 // データの処理 //////////////////////////////////////////////////////////////////////////////
 
 function connectMainSheet() {
-    // GoogleスプレッドシートのAPIを使ってアクティブなスプレッドシートを取得
-    const spread = SpreadsheetApp.getActiveSpreadsheet();
+    const active_spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     // スプレッドシート内の"メイン" という名前のシートを取得
-    const main_sheet = spread.getSheetByName("メイン");
+    const main_sheet = active_spreadsheet.getSheetByName("メイン");
     return main_sheet;
 }
 
 function fetchKanfiData(mainSheet) {
     // アンケートデータが存在するスプレッドシートのURLを取得し、そのURLを使ってシートを取得
     const form_url = mainSheet.getRange("b3").getValue();
-    // 感フィのスライドのURLを取得し、そのURLを使ってプレゼンテーションを開く
     const kanfi_form_sheet = SpreadsheetApp.openByUrl(form_url).getActiveSheet();
 
     // let xxx_source... : データの取得
-    // xxx_source=xxx_source.map... : データの整形(空白の削除)
+    // xxx_source=xxx_source.map... : データの整形(空白の削除)。
+    //      mapメソッドは配列の各要素に対して関数を実行し、その結果を新しい配列として返す。
+    //      =>はアロー関数。引数を受け取り、その引数を元にして何かを返す関数を簡潔に書くための構文。
 
+    // 1行目の4列目から最終列まで（フォームの質問に書かれているメンバーの名前）
     let member_name_source = kanfi_form_sheet.getRange(1, 4, 1, kanfi_form_sheet.getLastColumn() - 3).getValues()[0];
     member_name_source = member_name_source.map(
         name => deleteSpace(name)
     );
 
+    // フォームへの回答の2列目と3列目から、名前と読んで欲しい名前のペアを取得
     let nickname_source = kanfi_form_sheet.getRange(2, 2, kanfi_form_sheet.getLastRow() - 1, 2).getValues();
     nickname_source = nickname_source.map(
         row => row.map(text => deleteSpace(text))
     );
 
+    // 2行目以降、4列目以降から、メンバーへのメッセージを取得
     let message_source = kanfi_form_sheet.getRange(2, 4, kanfi_form_sheet.getLastRow() - 1, kanfi_form_sheet.getLastColumn() - 3).getValues();
     message_source = message_source.map(
         row => row.map(message => deleteSpace(message))
@@ -84,34 +88,34 @@ function fetchKanfiData(mainSheet) {
 function makeMembersMap(member_name_source) {
     const members = new Map();
     for (const name of member_name_source) {
-        members.set(name, new Member(name));
+        members.set(name, new Member(name)); // Mapオブジェクトの操作はset/getで行うことに注意
     };
     return members;
 }
 
-function setNickNames(nickname_source, members) {
+function setNicknames(nickname_source, members) {
     for (const row of nickname_source) {
         const name = row[0];
-        const target_member = members.get(name);
-
+        const target_member = members.get(name); // Mapオブジェクトの操作はset/getで行うことに注意
         const nickname = row[1];
         target_member.nickname = nickname;
     };
-    // 正しいニックネームが設定されているか確認
+    // 正しいニックネームが設定されているか確認するためのコード。
     // for (const name in members) {
     //     console.log(members[name].nickname); // テスト用なので、コンソールに出力すればOK
     // };
 }
 
 function setMessages(message_source, members) {
+    // membersの追加順とmessage_sourceの並び順が一致していることを前提としている。
+    // 高々数十人のメンバーなので、計算量は気にしないでいい。
     let i = 0;
     for (const member of members.values()) {
-        // membersの並び順とmessage_sourceの並び順が一致していることを前提としている
-        member.messages = message_source.map(function (row) {
-            return deleteSpace(row[i]);
-        }).filter(function (message) {
-            return message !== "";
-        });
+        member.messages = message_source.map(
+            row => deleteSpace(row[i])
+        ).filter(
+            message => message !== '' // 空でないメッセージのみを抽出
+        );
         i++;
     }
     // 正しいメッセージが設定されているか確認するためのコード。
@@ -123,7 +127,7 @@ function setMessages(message_source, members) {
 // スライドの処理 //////////////////////////////////////////////////////////////////////////////
 
 function connectKanfiSlide(mainSheet) {
-    // D3のセルに記載されているURLを使ってスライドを開く
+    // D3のセルに感フィのスライドのURLが記載されている。
     const kanfi_url = mainSheet.getRange("d3").getValue();
     if (deleteSpace(kanfi_url) == "") {
         // URLが空の場合は新しいスライドを作成
@@ -138,8 +142,7 @@ function connectKanfiSlide(mainSheet) {
     return SlidesApp.openByUrl(kanfi_url);
 }
 
-
-function assignExsistingSlidestoMembers(members, kanfi_slide) {
+function assignExsistingSlidesToMembers(members, kanfi_slide) {
     // 各メンバーのスライドの識別子を格納する集合を作成
     const set_of_notes = new Set();
     for (const member of members.values()) {
@@ -149,23 +152,16 @@ function assignExsistingSlidestoMembers(members, kanfi_slide) {
     }
 
     const exsisting_slides = kanfi_slide.getSlides();
-    const slides_to_remove = []; //  todo：削除リストが必要かどうかテスト
     for (const slide of exsisting_slides) {
         const note = deleteSpace(slide.getNotesPage().getSpeakerNotesShape().getText().asString()); // スライドのスピーカーノートを取得
         if (set_of_notes.has(note)) {
             // スピーカーノートが識別子リストに含まれている場合は、スライドを保存
             setSlideFromNote(note, slide, members);
         } else {
-            // そうでない場合はスライドを削除リストに追加
             log("スピーカーノート「" + note + "」のスライドを削除");
-            slides_to_remove.push(slide);
+            slide.remove(); // 削除したくない場合は、この2行をコメントアウト
         }
     }
-    for (const slide of slides_to_remove) {
-        // 削除リストに含まれるスライドを実際に削除
-        // テスト時にはVer1のコードで作成したスライドを用いるといい。
-        // slide.remove();
-    };
 }
 
 function setSlideFromNote(note, slide, members) {
@@ -183,16 +179,16 @@ function firstSlide(member, kanfi_slide) {
 }
 
 function secondSlide(member, kanfi_slide) {
-    initSlide(memeber, kanfi_slide, 1)
-    putMessagesOnSlide(member.messages, member.slide2);
+    initSlide(member, kanfi_slide, 1)
+    putMessagesOnSlide(member.messages, member.slides[1]);
 }
 
 function initSlide(member, kanfi_slide, slide_index) {
-    const target_slide = member.slides[slide_index];
+    let target_slide = member.slides[slide_index];
     if (target_slide == null) {
         target_slide = kanfi_slide.appendSlide();
         target_slide.getNotesPage().getSpeakerNotesShape().getText().setText(member.notes[slide_index]); // スピーカーノートを設定
-        target_slide.getBackground.setSolidFill("#000000"); // スライドの背景色を黒に設定
+        target_slide.getBackground().setSolidFill("#000000"); // スライドの背景色を黒に設定
         member.slides[slide_index] = target_slide;
     }
     putNicknameOnSlide(member.name, member.nickname, target_slide);
@@ -200,11 +196,11 @@ function initSlide(member, kanfi_slide, slide_index) {
 }
 
 function putNicknameOnSlide(name, nickname, slide) {
-    const shapes_list = slide.getShapes();
-    for (const shape of shapes_list) {
-        if (deleteSpace(shape.getText().asString()) == nickname) {
-            // ニックネームがある場合はそのまま返す
-            return slide;
+    // ニックネームがある場合はそのまま返す
+    const textbox_list = slide.getShapes();
+    for (const textbox of textbox_list) {
+        if (deleteSpace(textbox.getText().asString()) == nickname) {
+            return;
         }
     };
 
@@ -215,15 +211,16 @@ function putNicknameOnSlide(name, nickname, slide) {
     textbox_content.getTextStyle().setForegroundColor("#ffffff").setFontSize(60);
     textbox_content.getParagraphStyle().setParagraphAlignment(SlidesApp.ParagraphAlignment.CENTER);
 
+    // このメンバーがフォームに回答する前に作成されたスライドには、初期値の本名のテキストボックスが存在する。
     // ニックネームでない本名のみのテキストボックスを削除。
     if (nickname != name) {
-        for (const shape of shapes_list) {
+        for (const shape of textbox_list) {
             if (deleteSpace(shape.getText().asString()) == name) {
                 shape.remove();
+                break;
             }
         };
     }
-    return slide;
 }
 
 function putMessagesOnSlide(message_list, slide) {
@@ -242,9 +239,11 @@ function putMessagesOnSlide(message_list, slide) {
 
 function putNewMessage(message, slide, i) {
     // 8メッセージで1列になるように配置
-    const x_coordinate = ((i - (i % 8)) / 8) * 150 + 450;
-    const y_coordinate = (i % 8) * 50;
-    const textbox = slide.insertShape(SlidesApp.ShapeType.TEXT_BOX, x_coordinate, y_coordinate);
+    const left = ((i - (i % 8)) / 8) * 150 + 450; // 左からの位置
+    const top = (i % 8) * 50; // 上からの位置
+    const width = 200;
+    const height = 50;
+    const textbox = slide.insertShape(SlidesApp.ShapeType.TEXT_BOX, left, top, width, height);
 
     const textbox_content = textbox.getText();
     textbox_content.setText(message);
@@ -255,7 +254,7 @@ function putNewMessage(message, slide, i) {
     style.setFontSize(20);
 }
 
-// その他の処理 //////////////////////////////////////////////////////////////////////////////
+// ユーティリティ //////////////////////////////////////////////////////////////////////////////
 
 function createLogger(loggerCell) {
     // ログデータを記録する関数を作成する関数
@@ -271,23 +270,20 @@ function deleteSpace(text) {
 }
 
 function getRandomRGB() {
-    // ランダムな色相
-    const hue = Math.floor(Math.random() * 360);
-    // 最大彩度
-    const saturation = 100;
-    // 最大明度
-    const brightness = 100;
+    const hue = Math.floor(Math.random() * 360); // ランダムな色相
+    const saturation = 100; // 最大彩度
+    const brightness = 100; // 最大明度
 
     // RGBに変換
     const rgb = hsvToRgb(hue, saturation, brightness);
 
     // カラーコードを生成
     const rgb_code = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+    const rgb_hex = rgbToHex(rgb);
 
-    return rgb_code;
+    return rgb_hex;
 }
 
-// HSBからRGBに変換
 function hsvToRgb(hue, saturation, brightness) {
     const h = hue / 360;
     const s = saturation / 100;
@@ -333,4 +329,12 @@ function hsvToRgb(hue, saturation, brightness) {
     };
 
     return rgb;
+}
+
+function rgbToHex(rgb) {
+    const r = rgb.r.toString(16).padStart(2, '0');
+    const g = rgb.g.toString(16).padStart(2, '0');
+    const b = rgb.b.toString(16).padStart(2, '0');
+
+    return `#${r}${g}${b}`;
 }
